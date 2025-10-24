b85ddee7aaf5690ef6ad4e4adf7160e963f37c10
Cgines-stech <cgines@stech.edu>
2025-10-24 11:50:10 -0600

    update

--- data/programs/Automation Technology/TEAM 1020.js
+++ data/programs/Automation Technology/TEAM 1020.js
@@ -65,21 +65,7 @@ const TEAM1020 = [
     ],
 
     course_Policies: [
-        {
-            title: "Course Grading",
-            content: [
-            "Students must achieve 80% (B-) or higher to pass graded work. Incomplete assignments must be redone to meet the required standards. Guidelines, rules, and expectations for completing assignments are provided in each course.",
-            "High School Power School Grades: Quarter student grades will be determined by student progress percentage. Faculty will use the higher percentage of either 1) quarter progress, or 2) cumulative progress for the current training plan year."
-            ]
-            
-        },
-        gradeScale,
-        {
-            content2: [
-            "Course Policies: In alignment with industry standards, students shall not be permitted to use cell phones in the lab area, except when researching information pertinent to coursework. Safety Glasses must be worn in the lab. No Loose Clothing when operating Equipment, No Shorts, flip flops, or sandals. Possession or use of controlled substances or their imitations in the lab, which can impair judgment or emergency response, is a severe safety violation. This disregard for safety may result in immediate withdrawal from the program. No one may work in the lab with any substance, legal or otherwise, that may impair them in any way."
-            ]
-            
-        },
+        {},
     ],
 
     courseAssignmentsandAsssessments: [
