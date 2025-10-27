7a89e6df19d81bbbe7e1c0050cb1e0ec26bc3abd
Cgines-stech <cgines@stech.edu>
2025-10-27 09:10:17 -0600

    Update institutionalPolicy.js

--- data/institutionalPolicy.js
+++ data/institutionalPolicy.js
@@ -4,24 +4,17 @@ export default [
     title: "Campus Requirements",
     content: [
       `InformaCast Statement: Southwest Tech uses InformaCast to ensure the safety and well-being of our students. In times of emergency, such as weather closures and delays, this app allows us to promptly deliver notifications directly to your mobile devices. To stay informed and receive real-time updates, we encourage all students to sign up for notifications. Your safety is our priority, and staying connected ensures a swift response to any unforeseen circumstances. More information and directions for signing up are available at: <a href="https://stech.edu/emergency-notifications/">https://stech.edu/emergency-notifications/</a>`,
-
       `Internet Acceptable Use Policy: The student is expected to review and follow the Southwest Technical College Internet Safety Policy at: <a href="https://stech.edu/students/policies/">https://stech.edu/students/policies/</a>`,
-
       `Student Code of Conduct Policy: The student is expected to review and follow the Southwest Technical College Student Code of Conduct Policy at: <a href="https://stech.edu/students/policies/">https://stech.edu/students/policies/</a>`,
-
       `Accommodations: Students with medical, psychological, learning, or other disabilities desiring accommodations or services under ADA, must contact the Student Services Office. Student Services determines eligibility for and authorizes the provision of these accommodations and services. Students must voluntarily disclose that they have a disability, request an accommodation, and provide documentation of their disability. Students with disabilities may apply for accommodations, based on an eligible disability, through the Student Services office located at 757 W. 800 S., Cedar City, UT 84720, and by phone at (435) 586-2899. No diagnostic services are currently available through Southwest Technical College.`,
-
       `Safety and Building Maintenance: The College has developed and follows a variety of plans to ensure the safe and effective operation of its facilities and programs. The following plans are available online:<br>1) Facilities Operations and Maintenance Plan; 2) Technical Infrastructure Plan; and 3) Health and Safety Plan.`,
-
       `Withdrawals and Refunds: Please refer to the Southwest Technical College Refund Policy at: <a href="https://stech.edu/students/policies/">https://stech.edu/students/policies/</a>`,
-
       `Any high school or adult student, who declares a technical training objective is eligible for admission at Southwest Technical College (Southwest Tech). Program-specific admissions requirements may exist and will be listed on the Southwest Tech website. A high school diploma or equivalent is not required for admission but is mandatory for students seeking Title IV Federal Financial Aid.`,
-
-      `Non-Discriminatory Policy: Southwest Technical College affirms its commitment to promote the goals of fairness and equity in all aspects of the educational enterprise, and bases its policies on the idea of global human dignity.`
-      `Southwest Tech is committed to a policy of nondiscrimination. No otherwise qualified person may be excluded from participation in or be subjected to discrimination in any course, program or activity because of race, age, color, religion, sex, pregnancy, national origin or disability. Southwest Technical College does not discriminate on the basis of sex in the education programs or activities that it operates, as required by Title IX and 34 CFR part 106. The requirement not to discriminate in education programs or activities extends to admission and employment. Inquiries about Title IX and its regulations to STECH may be referred to the Title IX Coordinator, to the Department of Education, and/or to the Office for Civil rights.`
-      `If you believe you have experienced discrimination or harassment on our campus, please contact the Title IX Coordinator, Cory Estes: cestes@stech.edu, (435) 865-3938.`
-      `For special accommodations, please contact the ADA Coordinator, Cyndie Tracy: ctracy@stech.edu, (435) 865-3944.`
-      `Southwest Technical College<br>
+      `Non-Discriminatory Policy: Southwest Technical College affirms its commitment to promote the goals of fairness and equity in all aspects of the educational enterprise, and bases its policies on the idea of global human dignity.<br>
+      Southwest Tech is committed to a policy of nondiscrimination. No otherwise qualified person may be excluded from participation in or be subjected to discrimination in any course, program or activity because of race, age, color, religion, sex, pregnancy, national origin or disability. Southwest Technical College does not discriminate on the basis of sex in the education programs or activities that it operates, as required by Title IX and 34 CFR part 106. The requirement not to discriminate in education programs or activities extends to admission and employment. Inquiries about Title IX and its regulations to STECH may be referred to the Title IX Coordinator, to the Department of Education, and/or to the Office for Civil rights.<br><br>
+      If you believe you have experienced discrimination or harassment on our campus, please contact the Title IX Coordinator, Cory Estes: cestes@stech.edu, (435) 865-3938.<br>
+      For special accommodations, please contact the ADA Coordinator, Cyndie Tracy: ctracy@stech.edu, (435) 865-3944.<br>
+      Southwest Technical College<br>
       757 West 800 South<br>
       Cedar City, UT 84720<br>
       info@stech.edu<br>
