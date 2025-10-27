143cd4b162e69f1c0293525348898f5d42004921
Cgines-stech <cgines@stech.edu>
2025-10-27 08:43:40 -0600

    Update institutionalPolicy.js

--- data/institutionalPolicy.js
+++ data/institutionalPolicy.js
@@ -1,7 +1,7 @@
 // data/institutionalPolicy.js
 export default [
   {
-    title: "Additional Information",
+    title: "Campus Requirements",
     content: [
       `InformaCast Statement: Southwest Tech uses InformaCast to ensure the safety and well-being of our students. In times of emergency, such as weather closures and delays, this app allows us to promptly deliver notifications directly to your mobile devices. To stay informed and receive real-time updates, we encourage all students to sign up for notifications. Your safety is our priority, and staying connected ensures a swift response to any unforeseen circumstances. More information and directions for signing up are available at: <a href="https://stech.edu/emergency-notifications/">https://stech.edu/emergency-notifications/</a>`,
 
