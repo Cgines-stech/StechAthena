bcbb9e88b0833ca6392ffd45f47a331ff1e69998
Cgines-stech <cgines@stech.edu>
2025-10-28 12:39:31 -0600

    Update firebase.js

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -2,25 +2,21 @@
 import { firebaseConfig } from "./firebase-config.js";
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-app.js";
 import {
-  getAuth, GoogleAuthProvider, signInWithPopup, onAuthStateChanged, signOut
+  getAuth, signInAnonymously, onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-auth.js";
 import {
   getFirestore, serverTimestamp
 } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-firestore.js";
 
-// Init
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-
-// Google auth helpers
-const provider = new GoogleAuthProvider();
-provider.setCustomParameters({ prompt: "select_account" });
-
-export async function signInWithGoogle() {
-  const res = await signInWithPopup(auth, provider);
-  return res.user;
-}
-export async function signOutNow() { await signOut(auth); }
-export function onAuth(cb) { return onAuthStateChanged(auth, cb); }
 export { serverTimestamp };
+
+// 🔇 Zero-UI auth: silently sign in
+onAuthStateChanged(auth, (u) => {
+  if (!u) {
+    // Fire and forget; you don't need to await this for the page to work
+    signInAnonymously(auth).catch(console.error);
+  }
+});
