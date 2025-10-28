9ee08fc791a415a561040576b0a50e21efc3114b
Cgines-stech <cgines@stech.edu>
2025-10-28 12:51:17 -0600

    Update firebase.js

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -2,11 +2,15 @@
 import { firebaseConfig } from "./firebase-config.js";
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-app.js";
 import {
-  getAuth, signInAnonymously, onAuthStateChanged
+  getAuth,
+  signInAnonymously,   // ✅ must be imported here
+  onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-auth.js";
 import {
-  getFirestore, serverTimestamp
+  getFirestore,
+  serverTimestamp
 } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-firestore.js";
+
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
