a15d3c1f53af10999658d77c3cc2ff771e6ad3bb
Cgines-stech <cgines@stech.edu>
2025-11-04 06:34:37 -0700

    Update programs.js

--- data/programs.js
+++ data/programs.js
@@ -6,6 +6,6 @@ export const PROGRAMS = [
   { id: 400, department_id: 4, name: "Commercial Driver's License Class A", program_credit_hours: 6, CIP: 49.0205},
   { id: 500, department_id: 5, name: "Culinary Arts", program_credit_hours: 30, CIP: 12.0503},
   { id: 600, department_id: 2, name: "Electrical Apprenticeship", program_credit_hours: 24, CIP: 46.0302},
-  { id: 120, department_id: 1, name: 'Emergency Medical Technician', program_credit_hours: 34, CIP: 51.0904},
+  { id: 120, department_id: 1, name: 'Emergency Medical Technician', program_credit_hours: 6, CIP: 51.0904},
   { id: 110, department_id: 1, name: 'Paramedic', program_credit_hours: 34, CIP: 51.0904},
 ];
