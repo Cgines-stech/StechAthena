72ceb82f2646b69a859ffae9699acc8157667627
Cgines-stech <cgines@stech.edu>
2025-12-05 14:34:47 -0700

    update

---
+++ data/programs/Surgical Technology/TESU 1221.js
@@ -0,0 +1,96 @@
+// TESU 1221
+
+// imports
+import gradeScale from "../../gradescale.js";
+import { courseTotals, sum, money } from "../../utils/helpers.js";
+
+const GS = Array.isArray(gradeScale) ? gradeScale[0] : gradeScale;
+
+const TESU1221 = [
+  {
+    id: 19010,
+    programId: 1,
+    courseNumber: "TESU 1221",
+    courseName: "Surgical Technology Seminar",
+    courseCredits: 1,
+    statewideAlignment: "Non-Aligned",
+    courseClockHours: 30,
+    courseTuition: 95.00,
+    courseFee: 0.00,
+    instructionalType: "LEC",
+    isElective: false,
+    includeInProgramTotals: true,
+
+    courseDescription:
+      "The Surgical Technology Seminar course will discuss factors associated with making career decisions that can enhance a surgical technologist’s professional growth and success. This course will also review competencies learned throughout the program in order to sit for the national certification exam.",
+
+    courseObjectives: [
+      "Review for the national certifying exam",
+      "Prepare students for employment",
+    ],
+
+    courseOutline: [
+      { title: "Preparation for Employment", hours: 30},
+    ],
+
+    courseBooks: [
+      {},
+    ],
+
+    syllabiBooks: [
+      {},
+    ],
+    
+    courseTools: [
+      {},
+    ],
+
+    courseCertifications: [
+      {
+         name: "BLS",
+         price: 45.0,
+         note: ""
+      }
+    ],
+
+    otherAssociatedCosts: [
+      {}
+    ],
+
+    courseClassroomHours: [
+      {
+        startDate: "11/16/2026",
+        endDate: "11/20/2026",
+        Monday: "8:00 AM - 3:00 PM",
+        Tuesday: "8:00 AM - 3:00 PM",
+        Wednesday: "8:00 AM - 3:00 PM",
+        Thursday: "8:00 AM - 3:00 PM",
+        Friday: "8:00 AM - 10:00 AM",
+        Saturday: "",
+      }
+    ],
+
+    instructorContactInformation: [
+      {}
+    ],
+
+    course_Policies: [
+      {},
+    ],
+
+    courseAssignmentsandAsssessments: [
+      "Orientation",
+      "TESU 1221 Syllabi",
+      "Surgical Technology Seminar",
+      "Section 1: Introduction to Surgical Technology",
+      "Chapter 1: Orientation to Surgical Technology",
+      "Read",
+      "Orientation to Surgical Technology",
+      "Resume",
+      "What makes a good scrub tech?",
+      "End of Course Survey",
+    ]
+  }
+]
+
+export default TESU1221;
--- data/programs/Surgical Technology/TESU 2910.js
+++ data/programs/Surgical Technology/TESU 2910.js
@@ -11,7 +11,7 @@ const TESU2910 = [
     id: 19009,
     programId: 1,
     courseNumber: "TESU 2910",
-    courseName: "Surgical Technology Clinical Externship I",
+    courseName: "Surgical Technology Clinical Externship II",
     courseCredits: 4,
     statewideAlignment: "Aligned",
     courseClockHours: 180,
@@ -22,7 +22,7 @@ const TESU2910 = [
     includeInProgramTotals: true,
 
     courseDescription:
-      "The Surgical Technology Clinical Externship I course will extend the educational experience in the surgical field under the supervision of experienced medical professionals in a clinical setting.",
+      "The Surgical Technology Clinical Externship II course will extend the educational experience in the surgical field under the supervision of experienced medical professionals in a clinical setting.",
 
     courseObjectives: [
       "Practice decontamination standards and procedures for both sterile and non-sterile instrumentation and equipment",
@@ -35,10 +35,10 @@ const TESU2910 = [
     ],
 
     courseOutline: [
-      { title: "Surgical Technology Clinical Externship I", hours: 45},
-      { title: "Surgical Technology Clinical Externship II", hours: 45},
-      { title: "Surgical Technology Clinical Externship III", hours: 45},
-      { title: "Surgical Technology Clinical Externship IV", hours: 45},
+      { title: "Surgical Technology Clinical Externship V", hours: 45},
+      { title: "Surgical Technology Clinical Externship VI", hours: 45},
+      { title: "Surgical Technology Clinical Externship VII", hours: 45},
+      { title: "Surgical Technology Clinical Externship VIII", hours: 45},
     ],
 
     courseBooks: [
@@ -54,11 +54,7 @@ const TESU2910 = [
     ],
 
     courseCertifications: [
-      {
-         name: "NCCT TS-C Exam",
-         price: 199.0,
-         note: ""
-      }
+      {}
     ],
 
     otherAssociatedCosts: [
@@ -67,8 +63,8 @@ const TESU2910 = [
 
     courseClassroomHours: [
       {
-        startDate: "05/01/2026",
-        endDate: "07/17/2026",
+        startDate: "07/20/2026",
+        endDate: "10/30/2026",
         Monday: "",
         Tuesday: "",
         Wednesday: "",
@@ -83,33 +79,49 @@ const TESU2910 = [
     ],
 
     course_Policies: [
-      {},
+      {
+        title: "Course Grading",
+        content: [
+          "A clinical evaluation document must be submitted for each clinical day to count towards your overall hours and grades. Clinical evaluation documents will be graded according to how much of the document is completed and that the information is accurate. Students must maintain 80% in this course."
+        ]  
+      },
+      {
+        type: "gradeScale",
+        title: GS?.title || "Grading Scale",
+         description: GS?.description || "",
+        scale: GS?.scale || []   // expecting 12 items with { letter, range }
+      },
+      {
+        title: "General Course Policies",
+        content: [
+          "Course Policies: Student must adhere to the Program Safety, Performance Standards, and code of conduct outlined in the Surgical Technology Policies and Procedures. Student must wear royal blue embroidered scrubs and close-toed shoes and adhere to facility policies and standards.",
+          "Students must complete all required hours and surgical cases to be eligible for the certification exam. Students must complete all of their clinical hours in a timely manner. Each student is required to complete approximately 16 hours of clinicals per week. If you are going to be absent, communication with your instructor will be required to help you set up time to make up clinical hours. It is your responsibility to ask for more clinical days if you do not have enough hours. Make-up days will be provided towards the end of the course. If clinical hours are not completed before the end date, you may be dismissed from the program. You may use cell phones at your own discretion. We highly recommend that you avoid taking your cell phone with you into the operating room.",
+          "You may be required to stand for a long amount of hours without a break. You may need to lift heavy items such as instrument pans or transferring a patient.",
+        ]  
+      },
     ],
 
     courseAssignmentsandAsssessments: [
       "Orientation",
-      "TESU 2900 Syllabi",
+      "TESU 2910 Syllabi",
       "Student Clinical Paperwork.pdf",
-      "Clinical Information for Students.pdf",
+      "Clinical Information for Student s.pdf",
       "Clinical Schedule 2025.pdf",
       "Cedar City Clinical Sheet.pdf",
       "St.George Regional Clinical Sheet.pdf",
-      "Week 1/2",
+      "Week 1",
+      "Week 2",
       "Week 3",
       "Week 4",
       "Week S",
       "Week 6",
       "Week 7",
-      "Experience Essay",
       "Week 8",
       "Week 9",
       "Week 10",
-      "Week 11",
-      "Week 12",
-      "Week 13",
       "Makeup Days",
+      "Critical Competency for Certification",
       "Clinical Preparation Sheets",
-      "Clinical Experience Essay",
       "End of Course Survey",
     ]
   }
