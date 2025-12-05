aa371bb766a03fe6c14c45e68faf2a68259fb5f7
Cgines-stech <cgines@stech.edu>
2025-12-05 14:55:46 -0700

    update

--- data/programs/Software Development/classRoomDates.js
+++ data/programs/Software Development/classRoomDates.js
@@ -4,11 +4,30 @@ export default [
   {
     startDate: "",
     endDate: "",
-    Monday: ["8:00 AM - 11:00 AM, 12:00 PM - 3:00 PM, 4:00 PM - 7:00 PM"],
-    Tuesday: ["8:00 AM - 11:00 AM, 12:00 PM - 3:00 PM, 4:00 PM - 7:00 PM"],
-    Wednesday: ["8:00 AM - 11:00 AM, 12:00 PM - 3:00 PM, 4:00 PM - 7:00 PM"],
-    Thursday: ["8:00 AM - 11:00 AM, 12:00 PM - 3:00 PM, 4:00 PM - 7:00 PM"],
-    Friday: ["8:00 AM - 11:00 AM, 12:00 PM - 3:00 PM"],
-    Saturday: "",
+    Monday: [
+      "8:00 AM - 11:00 AM",
+      "12:00 PM - 3:00 PM",
+      "4:00 PM - 7:00 PM",
+    ],
+    Tuesday: [
+      "8:00 AM - 11:00 AM",
+      "12:00 PM - 3:00 PM",
+      "4:00 PM - 7:00 PM",
+    ],
+    Wednesday: [
+      "8:00 AM - 11:00 AM",
+      "12:00 PM - 3:00 PM",
+      "4:00 PM - 7:00 PM",
+    ],
+    Thursday: [
+      "8:00 AM - 11:00 AM",
+      "12:00 PM - 3:00 PM",
+      "4:00 PM - 7:00 PM",
+    ],
+    Friday: [
+      "8:00 AM - 11:00 AM",
+      "12:00 PM - 3:00 PM",
+    ],
+    Saturday: [], // no hours instead of ""
   }
 ];
