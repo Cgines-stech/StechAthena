104476f83ff0eabedff0e550c9d3fb20e186572b
Cgines-stech <cgines@stech.edu>
2025-12-08 07:50:46 -0700

    Create TEWT 1045.js

---
+++ data/programs/Welding Technology/TEWT 1045.js
@@ -0,0 +1,187 @@
+// TEWT 1045
+
+// imports
+import gradeScale from "../../gradescale.js";
+import { courseTotals, sum, money } from "../../utils/helpers.js";
+
+const TEWT1045 = [
+  {
+    id: 95002,
+    programId: 1,
+    courseNumber: "TEWT 1045",
+    courseName: "Inspection, Metallurgy, and Blueprints",
+    courseCredits: 2,
+    statewideAlignment: "Non-Aligned",
+    courseClockHours: 60,
+    courseTuition: 190.00,
+    courseFee: 10.00,
+    instructionalType: "LEL",
+    isElective: false,
+    includeInProgramTotals: true,
+
+    courseDescription:
+      "This course will introduce students to welding symbols and blueprints that welding professionals use. Discussing different welding processes and materials that can be welded. Basic AWS standards and types of nondestructive testing (NDT) and destructive testing will be covered.",
+
+    courseObjectives: [
+      "Discuss codes, standards, and types of nondestructive and destructive testing.",
+      "Identify and interpret basic AWS standardized welding symbols, blueprints, and bill of materials.",
+      "Identify parts of a joint, parts of a weld, and how they correspond with blueprints.",
+      "Identify and state various stresses a weld can be subjected to.",
+      "Explain welder qualifications, certification, and welding procedure specifications (WPS).",
+    ],
+
+    courseOutline: [
+      { title: "Weld Symbols and Welding Symbols", hours: 10},
+      { title: "Print Reading", hours: 10},
+      { title: "Codes, Standards and Welding Procedure Specification", hours: 10},
+      { title: "Destructive and Non-destructive Testing", hours: 10},
+      { title: "Welding Metallurgy", hours: 10},
+      { title: "Weldability of Metals and Electrodes", hours: 10},
+    ],
+
+    courseBooks: [
+      {},
+    ],
+
+    syllabiBooks: [
+      {},
+    ],
+    
+    courseTools: [
+      {},
+    ],
+
+    courseCertifications: [
+      {},
+    ],
+
+    otherAssociatedCosts: [
+      {}
+    ],
+
+    courseClassroomHours: [
+      {}
+    ],
+
+    instructorContactInformation: [
+      {},
+    ],
+
+    course_Policies: [
+      {}
+    ],
+
+    courseAssignmentsandAsssessments: [
+      "Work Place Practices- Importance of Blueprints",
+      "READ Chapter 22: Welding Joint Design and Welding",
+      "Symbols",
+      "LEARN IT: Chapter 22: Overview",
+      "Chapter 22: Review Questions",
+      "Chapter 22 Practice Test",
+      "Practice 22-1: Reading Welding Symbols",
+      "Practice 22-2: Reading Welding Symbols",
+      "Grooves and Fillet Welds",
+      "Weld Plate Work Sheet Joints and Welding Positions",
+      "Types of Welds- Website to Read",
+      "Chapter 22: Flashcards",
+      "READ Welding in the Real World",
+      "Checkpoint Meeting Module 2",
+      "READ Chapter 24: Welding Codes and Standards",
+      "LEARN IT: Chapter 24: Overview",
+      "Chapter 24: Review Questions",
+      "Chapter 24 Practice Test",
+      "Example of Procedure Qualification Record",
+      "Welding Procedure Specification",
+      "Practice 24-1: Writing a Welding Procedure Specification",
+      "(WPS)",
+      "Practice 24-2: Procedure Qualification Record (PQR)",
+      "READ Welding in the Real World",
+      "Chapter 24: Flashcards",
+      "Checkpoint Meeting Module 3",
+      "READ Chapter 25: Testing and Inspection",
+      "LEARN IT: Chapter 25: Overview",
+      "Chapter 25: Review Questions",
+      "Chapter 25 Practice Test",
+      "Welding Defects",
+      "Weld Discontinuities",
+      "Weld Plate Work Sheet Identifying Discontinuities",
+      "READ Welding in the Real World",
+      "Chapter 25: Flashcards",
+      "Checkpoint Meeting Module 4",
+      "READ Chapter 25: Testing and Inspection",
+      "LEARN IT: Chapter 25: Overview",
+      "Chapter 25: Review Questions",
+      "Chapter 25 Practice Test",
+      "Caliper",
+      "Ultrasound Testing",
+      "V-WACGauge",
+      "Fillet Weld Gauge",
+      "Misc tools",
+      "Weld Plate Work Sheet Using Inspection Tools",
+      "Chapter 25: Flashcards",
+      "READ Welding in the Real World",
+      "Checkpoint Meeting Module 5",
+      "Welding w ithout a Table",
+      "Weld Sequencing",
+      "Large Scale Squaring",
+      "Tape Measures",
+      "Levels",
+      "Making Marks on Metals",
+      "Checkpoint Meeting Module 6",
+      "READ Chapter 26: Welding Metallurgy",
+      "LEARN IT: Chapter 26: Overview",
+      "Chapter 26: Review Questions",
+      "Chapter 26 Practice Test",
+      "Steel Metallurgy - Principles of Metallurgy",
+      "Engineering Materials - Principles of Metallurgy",
+      "Real Engineering: The History of Iron and Steel",
+      "Properties and Grain Structure",
+      "Steel",
+      "American Steel",
+      "READ Welding in the Real World",
+      "Chapter 26: Flashcards",
+      "Checkpoint Meeting Module 7",
+      "READ Chapter 27: Weldability of Metals",
+      "LEARN IT: Chapter 27: Overview",
+      "Chapter 27: Review Questions",
+      "Chapter 27 Practice Test",
+      "The Difference Between Copper, Brass and Bronze",
+      "Copper, Brass & Bronze alloys explai ned",
+      "Metal- Modern Marvels",
+      "The Four Types of Steel- Metal Supermarkets",
+      "Stainless Steel Grades Explained",
+      "Stainless Forgotten History: A Revolution in Steel",
+      "Heavy Metals- Modern Marvels",
+      "Aluminum - The Material That Changed The World",
+      "How Stuff Works - Aluminum",
+      "Aluminium Alloys Explained",
+      "Aluminum- Modern Marvels",
+      "Cold Rolled VS Hot Rolled",
+      "READ Welding in the Real World",
+      "Chapter 27: Flashcards",
+      "Checkpoint Meeting Module 8",
+      "READ Chapter 28: Filler Metal Selection",
+      "LEARN IT: Chapter 28: Overview",
+      "Chapter 28: Review Questions",
+      "Chapter 28 Practice Test",
+      "Choosing the Right SMAW Electrode",
+      "5 Common Types Of Welding Filler Rods",
+      "Read through this text. A short video is included.",
+      "WIKI is also a good resource for answering questions and",
+      "providing additional",
+      "information.",
+      "READ Welding in the Real World",
+      "Chapter 28: Flashcards",
+      "Checkpoint Meeting Module 9",
+      "How to Read Welding Symbols: Part 1 - Fillet Welds",
+      "Symbols for Welding",
+      "Checkpoint Meeting Module 10",
+      "Blueprint Reading For Welders and Fitters",
+      "End of Course Survey",
+      "Work Ethic Assessment",
+      "Checkpoint Meeting Module 11",
+    ]
+  }
+]
+
+export default TEWT1045;
