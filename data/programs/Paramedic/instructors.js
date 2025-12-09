cc1c11c8f8a433d75562c3fc670a2cb1701bc146
Cgines-stech <cgines@stech.edu>
2025-12-09 13:29:27 -0700

    Update instructors.js

--- data/programs/Paramedic/instructors.js
+++ data/programs/Paramedic/instructors.js
@@ -1,9 +1,9 @@
 // data/programs/{program name}/instructors.js
 
 const paramedicInstructors = [
-  { name: "Nancy Small",        email: "nsmall@stech.edu",       title: "Instructor" },
-  { name: "Tyler March", email: "tmarch@stech.edu",   title: "Instructor" },
-  { name: "Matthew Goodman", email: "mgoodman@stech.edu",   title: "Instructor" },
+  { name: "Nancy Small",        email: "nsmall@stech.edu",       title: "" },
+  { name: "Tyler March", email: "tmarch@stech.edu",   title: "" },
+  { name: "Matthew Goodman", email: "mgoodman@stech.edu",   title: "" },
 ];
 
 export default paramedicInstructors;
