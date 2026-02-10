6cba9e563fb70806925ed05354384dbe4b14f604
Cgines-stech <cgines@stech.edu>
2026-02-10 07:11:38 -0700

    update

--- data/programs/Commercial Driver's License Class A/instructors.js
+++ data/programs/Commercial Driver's License Class A/instructors.js
@@ -4,7 +4,6 @@ const cdlClassAInstructors = [
   { name: "Terrance Bresina",        email: "tbresina@stech.edu",       title: "Instructor" },
   { name: "James Coats", email: "jcoats@stech.edu",   title: "Instructor" },
   { name: "Christopher Macias", email: "cmacias@stech.edu",   title: "Instructor" },
-  { name: "James Visarraga", email: "jvisarraga@stech.edu",   title: "Instructor" },
   { name: "Chantry Brindley", email: "cbrindley@stech.edu",   title: "Instructor" },
 ];
 
