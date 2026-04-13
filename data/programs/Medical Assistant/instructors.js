b4685edab7922db133385212f27316120272f0c3
Cgines-stech <cgines@stech.edu>
2026-04-13 09:43:07 -0600

    update

--- data/programs/Medical Assistant/instructors.js
+++ data/programs/Medical Assistant/instructors.js
@@ -1,7 +1,7 @@
 // data/programs/Medical Assistant/instructors.js
 
 const medicalAssistantInstructors = [
-  { name: "Dusty Hunter",        email: "dhunter@stech.edu",       title: "" },
+  { name: "Dusty Hunter", email: "dhunter@stech.edu", title: "" },
   { name: "Candice Cox", email: "ccox@stech.edu",   title: "" },
 ];
 
