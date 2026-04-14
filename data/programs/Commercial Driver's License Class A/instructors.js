e0bbe6186d3da50014c18338f4ea1d254c31e3d8
Cgines-stech <cgines@stech.edu>
2026-04-14 07:28:07 -0600

    Update instructors.js

--- data/programs/Commercial Driver's License Class A/instructors.js
+++ data/programs/Commercial Driver's License Class A/instructors.js
@@ -1,10 +1,10 @@
 // data/programs/Commercial Driver's License Class A/instructors.js
 
 const cdlClassAInstructors = [
-  { name: "Terrance Bresina",        email: "tbresina@stech.edu",       title: "" },
   { name: "James Coats", email: "jcoats@stech.edu",   title: "" },
   { name: "Christopher Macias", email: "cmacias@stech.edu",   title: "" },
   { name: "Chantry Brindley", email: "cbrindley@stech.edu",   title: "" },
+  { name: "Terrance Bresina", email: "tbresina@stech.edu", title: "" },
 ];
 
 export default cdlClassAInstructors;
