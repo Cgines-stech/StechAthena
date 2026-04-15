5cf8c28eab2e3e17b08952d2c6e41b4362ac6b9f
Cgines-stech <cgines@stech.edu>
2026-04-15 10:44:49 -0600

    Update instructors.js

--- data/programs/Master Esthetician/instructors.js
+++ data/programs/Master Esthetician/instructors.js
@@ -2,7 +2,6 @@
 
 const masterEstheticianInstructors = [
   { name: "Roshni Mehta", email: "rmehta@stech.edu", title: "" },
-  { name: "Samantha Joyce Bassarear", email: "sbassarear@stech.edu",   title: "" },
   { name: "April Terry", email: "aterry@stech.edu",   title: "" },
 ];
 
