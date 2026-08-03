4a12b77a59787a700ee44f9a28a676f28a35e906
Cgines-stech <cgines@stech.edu>
2026-08-03 13:50:05 -0600

    Update instructors.js

--- data/programs/Information Technology/instructors.js
+++ data/programs/Information Technology/instructors.js
@@ -2,7 +2,6 @@
 
 const informationTechnologyInstructors = [
   { name: "Greg Davis", email: "gdavis@stech.edu", title: "" },
-  { name: "Austin Prince", email: "aprince@stech.edu",   title: "" },
 ];
 
 export default informationTechnologyInstructors;
